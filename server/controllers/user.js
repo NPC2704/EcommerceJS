172f666b773ae2cf33bba650164ec09091aa7ed2
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-04-29 01:59:43 +0700

    up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,12 +8,42 @@ const register = asyncHandler(async (req, res) => {
       sucess: false,
       mes: "Missing inputs",
     });
-  const respon = await User.create(req.body);
-  return res.status(200).json({
-    sucess: respon ? true : false,
-    respon,
-  });
+
+  // kiem tra email da dc dang ky chua
+  const user = await User.findOne({ email });
+  if (user) throw new Error("User has existed");
+  else {
+    const newUser = await User.create(req.body);
+    return res.status(200).json({
+      sucess: newUser ? true : false,
+      mes: newUser
+        ? "Register is successfully. Please go login~"
+        : "Something went wrong",
+    });
+  }
+});
+
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({
+      sucess: false,
+      mes: "Missing inputs",
+    });
+  // plain object
+  const response = await User.findOne({ email });
+  if (response && (await response.isCorrectPassword(password))) {
+    // Tách password và role ra khỏi response
+    const { password, role, refreshToken, ...userData } = response.toObject();
+    return res.status(200).json({
+      sucess: true,
+      userData: response,
+    });
+  } else {
+    throw new Error("Invalid credentials!");
+  }
 });
 module.exports = {
   register,
+  login,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const ctrls = require("../controllers/user");
 
 router.post("/register", ctrls.register);
-
+router.post("/login", ctrls.login);
 module.exports = router;
 
 // CRUD | Create - Read - Update - Delete | POST - GET - PUT - DELETEeee
